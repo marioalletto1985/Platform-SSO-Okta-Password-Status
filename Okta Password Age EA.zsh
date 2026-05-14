6b514acc33becc6a84b38f3b47b7d9f29cbd2dff
Mario Alletto <148086909+marioalletto1985@users.noreply.github.com>
2026-05-14 15:01:37 +0100

    Fix echo statement for result output

---
+++ Okta Password Age EA.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# Extension Attribute: Okta Password Age (Days)
+PLIST="/Library/Application Support/Zilch/com.zilch.passwordstatus.plist"
+if [[ -f "$PLIST" ]]; then
+    result=$(/usr/libexec/PlistBuddy -c "print :PasswordAgeDays" "$PLIST" 2>/dev/null)
+    [[ -z "$result" ]] && result="Unknown"
+else
+    result="Not Available"
+fi
+echo "<result>${result}</result>"
