9208f7ea4913009a5d582cfb611d2326b4c999c1
Mario Alletto <148086909+marioalletto1985@users.noreply.github.com>
2026-05-14 15:03:15 +0100

    Update Okta Password Age EA.zsh

--- Okta Password Age EA.zsh
+++ Okta Password Age EA.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 # Extension Attribute: Okta Password Age (Days)
-PLIST="/Library/Application Support/Zilch/com.zilch.passwordstatus.plist"
+PLIST="/Library/Application Support/Yourorg/com.yourorg.passwordstatus.plist""
 if [[ -f "$PLIST" ]]; then
     result=$(/usr/libexec/PlistBuddy -c "print :PasswordAgeDays" "$PLIST" 2>/dev/null)
     [[ -z "$result" ]] && result="Unknown"
